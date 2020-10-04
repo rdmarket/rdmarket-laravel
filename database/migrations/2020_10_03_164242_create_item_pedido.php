bc0153e2254d0e4e152158f927da01ef4fecfbd4
Joao Aurelio <joao_junior174@outlook.com>
2020-10-03 22:15:55 -0300

    commit da priscila

---
+++ database/migrations/2020_10_03_164242_create_item_pedido.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemPedido extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Item_Pedido', function (Blueprint $table) {
+            $table->bigInteger('nr_item_pedido');
+            $table->bigInteger('id_pedido');
+            $table->bigInteger('id_produto');
+            $table->bigInteger('qtd_item_produto');
+            $table->integer('cd_status_item_pedido');
+            $table->float('vlr_total_item_pedido');
+            $table->date('data_item_pedido');
+            $table->timestamps();
+            $table->primary(['nr_item_pedido','id_pedido']);
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Item_Pedido');
+    }
+}
