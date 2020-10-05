119bc0e3bdfe8080d63e451a22ed0bc79fbac839
Priscila Perez <priscilaperezpassos@outlook.com>
2020-10-05 19:52:38 -0300

    criação da BaseController

---
+++ app/Http/Controllers/admin/ClienteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClienteController extends BaseController
+{
+   public function __construct()
+   {
+        $this->classe = Cliente::class;
+        $this->view = 'admin.clientes';
+    
+   }
+}
