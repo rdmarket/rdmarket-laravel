91bdb4f4537fe89409d01194ef3c2b94d7515a66
Luzia Gabrielly <luzia.gabrielly@gmail.com>
2020-10-08 14:44:43 -0300

    luzia

---
+++ app/Http/Controllers/Api/PedidoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Models\Pedido;
+
+
+class PedidoController extends BaseController
+{
+     public function __construct()
+     {
+        $this->classe = Pedido::class;
+     }
+     
+}
