ef12aeeb94b902e17f03e72fafb33ab164a8b15b
Joao Aurelio <joao_junior174@outlook.com>
2020-10-28 17:31:59 -0300

    joao

--- resources/views/layout/includes/menu.blade.php
+++ resources/views/layout/includes/menu.blade.php
@@ -48,9 +48,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
-                </li>
+                </li> -->
             @else
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('home') }}">Home</a>
